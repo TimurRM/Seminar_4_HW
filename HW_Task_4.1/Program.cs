2230674944204f64b5a8707f6c9a2906750afa91
Timur <tumyp.paxumob@gmail.com>
2022-07-07 21:12:05 +0500

    Added Task_4.1

---
+++ HW_Task_4.1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int numberPow(int A, int B){
+
+    int pow = 1;
+    for (int i = 1; i <= B; i++){
+    pow *= A;
+    }
+    return pow;
+}
+
+Console.Write("Insert number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int numPow = numberPow(A, B);
+
+Console.WriteLine($"{A},{B} -> {numPow}");
+
+System.Console.WriteLine("--------------------");
+
+
+
+
+
+void PowNumber()
+{
+        // Console.Write("Insert number numA: ");
+        // int numA = Convert.ToInt32(Console.ReadLine());
+        // Console.Write("Insert number numB: ");
+        // int numB = Convert.ToInt32(Console.ReadLine());
+
+        int numA = new Random().Next(10);
+        int numB = new Random().Next(10);
+
+        Console.WriteLine($"{numA}, {numB} -> {Math.Pow(numA, numB)}");
+        
+    }
+    for (int i = 0; i < 3; i++)
+    {
+            PowNumber();
+}
